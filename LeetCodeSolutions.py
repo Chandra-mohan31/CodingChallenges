8c0d51562704b0e5db6656784210f344c045941b
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2021-04-26 13:43:19 +0530

    2nd

--- LeetCodeSolutions.py
+++ LeetCodeSolutions.py
@@ -56,30 +56,109 @@ class Solution:
 
 ############################################################
 
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        out = False;
+        out = False
         l = len(s)
-        
+
         stack = []
         for i in range(l):
-            if(s[i] in ["(","{","["]):
+            if(s[i] in ["(", "{", "["]):
                 stack.append(s[i])
-            
-            
-            elif "(" in stack  and s[i]==")" and stack[len(stack) - 1] == "(":
+
+            elif "(" in stack and s[i] == ")" and stack[len(stack) - 1] == "(":
                 stack.pop()
-            elif "[" in stack  and s[i]=="]":
+            elif "[" in stack and s[i] == "]":
                 stack.pop()
-            elif "{" in stack  and s[i]=="}":
+            elif "{" in stack and s[i] == "}":
                 stack.pop()
             else:
                 return False
-            
 
-        
-        if(len(stack)==0):
+        if(len(stack) == 0):
             out = True
         return out
 
 #############################################
+#    public static List<Integer> gradingStudents(List<Integer> grades) {
+#         List<Integer> roundedGrades = new ArrayList<Integer>();
+#         int t,x;
+#         for(int i=0;i<grades.size();i++){
+#             // System.out.println(grades.get(i));
+
+#             // roundedGrades.add(grades.get(i));
+#             x = grades.get(i);
+#             t = x;
+#             while (t%5!=0)
+#                 t++;
+#             // System.out.println(t);
+#             if(t-x<3 && x>=38)
+#             roundedGrades.add(t);
+#             else
+#             roundedGrades.add(x);
+#             }
+#         return roundedGrades;
+#     }
+
+# }
+
+######################################################
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        large_area = 0
+
+        for i in range(0, len(height)):
+            for j in range(i, len(height)):
+                large_area = max(large_area, min(
+                    height[i], height[j]) * (j - i))
+                # if(large_area < height[i]*abs(i-j)):
+                #     large_area = height[i]*abs(i-j)
+
+        return large_area
+#####################################
+
+# public class Solution {
+#     public int maxArea(int[] height) {
+#         int maxarea = 0, l = 0, r = height.length - 1;
+#         while (l < r) {
+#             maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+#             if (height[l] < height[r])
+#                 l++;
+#             else
+#                 r--;
+#         }
+#         return maxarea;
+#     }
+# }
+
+######################################################
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxx = 0
+        l = 0
+        r = len(height)-1
+        while(l < r):
+            maxx = max(min(height[r], height[l])*abs(r-l), maxx)
+            if height[r] > height[l]:
+                l = l+1
+            else:
+                r = r-1
+        return maxx
+
+################################################
+#         comb = list(combinations(nums, 3))
+#         ans = []
+        
+#         for i in comb:
+#             i = list(i)
+#             i.sort()
+#             if(sum(i) == 0 and i not in ans):
+#                 ans.append(i)
+        
+#         return ans
+
